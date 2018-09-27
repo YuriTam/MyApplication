f780b4f24b27f6235fdda4c2af43e462e63d258c
YuriTam <547161849@qq.com>
2018-09-27 15:52:38 +0800

    提交相关的工具类

---
+++ lib-utils/src/main/java/com/common/utils/VibrationUtils.java
@@ -0,0 +1,13 @@
+package com.common.utils;
+
+/**
+ * <pre>
+ *     author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2016/9/29
+ *     desc  :
+ * </pre>
+ */
+public class VibrationUtils {
+
+}
